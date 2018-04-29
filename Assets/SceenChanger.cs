51f78c8c55b848545553ac0527f802c6d69b8413
rhys <rhys.puddephatt@btinternet.com>
2018-04-29 19:20:36 +0100

    did a load of menue stuff and added animation

---
+++ Assets/SceenChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class SceenChanger : ScriptableObject
+{
+    public void ChangeScene(Object Scene, bool additive)
+    {
+        SceneManager.LoadScene(Scene.name, additive ? LoadSceneMode.Additive : LoadSceneMode.Single);
+    }
+    public void ChangeScene(Object Scene)
+    {
+        ChangeScene(Scene, false);
+    }
+}
---
+++ Assets/forcePointFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class forcePointFilter : MonoBehaviour
+{
+
+    Text t;
+    private void Awake()
+    {
+        t = GetComponent<Text>();
+        if (t)
+            t.mainTexture.filterMode = FilterMode.Point;
+
+    }
+}
