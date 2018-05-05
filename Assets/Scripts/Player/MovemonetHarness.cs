7b607fcdc4acb73603a88f0fce3952d1ffbb4aa7
rhys <rhys.puddephatt@btinternet.com>
2018-05-05 18:27:43 +0100

    i forgot the inertia cancelation needs to be both x AND y

--- Assets/Scripts/Player/MovemonetHarness.cs
+++ Assets/Scripts/Player/MovemonetHarness.cs
@@ -9,6 +9,8 @@ public class MovemonetHarness : MonoBehaviour
     [SerializeField]
     float Force = 1;
     private Vector2 LastDirectionMoved;
+    //onlu global for memory management;
+    private Vector2 newVelocity;
     // Use this for initialization
     void Awake()
     {
@@ -20,25 +22,32 @@ public class MovemonetHarness : MonoBehaviour
         direction = direction.normalized;
         if (rbody != null)
         {
+            newVelocity = rbody.velocity;
             //if the direction has changed and it hasnt changed from zero
             if (Mathf.Sign(LastDirectionMoved.x) != Mathf.Sign(direction.x) && LastDirectionMoved.x != 0)
             {
-                rbody.velocity = Vector2.zero;
+                newVelocity.x = 0;
             }
+            //if the direction has changed and it hasnt changed from zero
+            if (Mathf.Sign(LastDirectionMoved.y) != Mathf.Sign(direction.y) && LastDirectionMoved.y != 0)
+            {
+                newVelocity.y = 0;
+            }
+            rbody.velocity = newVelocity;
             rbody.AddForce(direction * Force * Time.fixedDeltaTime, ForceMode2D.Impulse);
         }
         else
             Debug.LogError("Movement Harness Missing Rigidbody2D", this.gameObject);
         LastDirectionMoved = direction;
-
     }
     private void FixedUpdate()
     {
         Vector2 Drag = Vector2.one;
         if (LastDirectionMoved.x == 0)
-            Drag.x /= 2;
+            Drag.x *= 0.95f;
         if (LastDirectionMoved.y == 0)
-            Drag.y /= 2;
+            Drag.y *= 0.95f;
+        
         rbody.velocity= rbody.velocity.ScaleChain(Drag);
 
 
