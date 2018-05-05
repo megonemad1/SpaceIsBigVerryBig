ee17d4ffc357dee0bec79390f663c7a220a73fc7
rhys <rhys.puddephatt@btinternet.com>
2018-05-05 18:53:27 +0100

    the inertia was still playing up so changed the checkking aproach

--- Assets/Scripts/Player/MovemonetHarness.cs
+++ Assets/Scripts/Player/MovemonetHarness.cs
@@ -8,6 +8,9 @@ public class MovemonetHarness : MonoBehaviour
     private Rigidbody2D rbody = null;
     [SerializeField]
     float Force = 1;
+    [SerializeField]
+    float dragAmount = 1;
+    [SerializeField]
     private Vector2 LastDirectionMoved;
     //onlu global for memory management;
     private Vector2 newVelocity;
@@ -23,13 +26,14 @@ public class MovemonetHarness : MonoBehaviour
         if (rbody != null)
         {
             newVelocity = rbody.velocity;
-            //if the direction has changed and it hasnt changed from zero
-            if (Mathf.Sign(LastDirectionMoved.x) != Mathf.Sign(direction.x) && LastDirectionMoved.x != 0)
+            //if the direction has changed
+
+            if (LastDirectionMoved.x>0 && direction.x<0 && direction.x !=0)
             {
                 newVelocity.x = 0;
             }
             //if the direction has changed and it hasnt changed from zero
-            if (Mathf.Sign(LastDirectionMoved.y) != Mathf.Sign(direction.y) && LastDirectionMoved.y != 0)
+            if (LastDirectionMoved.y > 0 && direction.y < 0 && direction.y != 0)
             {
                 newVelocity.y = 0;
             }
@@ -38,17 +42,22 @@ public class MovemonetHarness : MonoBehaviour
         }
         else
             Debug.LogError("Movement Harness Missing Rigidbody2D", this.gameObject);
+
+        
         LastDirectionMoved = direction;
     }
     private void FixedUpdate()
     {
-        Vector2 Drag = Vector2.one;
-        if (LastDirectionMoved.x == 0)
-            Drag.x *= 0.95f;
+        newVelocity = rbody.velocity;
+        if (LastDirectionMoved.x == 0f)
+        {
+          
+            newVelocity.x -= newVelocity.x * dragAmount * Time.fixedDeltaTime;
+        }
         if (LastDirectionMoved.y == 0)
-            Drag.y *= 0.95f;
-        
-        rbody.velocity= rbody.velocity.ScaleChain(Drag);
+            newVelocity.y -= newVelocity.y * dragAmount * Time.fixedDeltaTime;
+
+        rbody.velocity = newVelocity;
 
 
     }
