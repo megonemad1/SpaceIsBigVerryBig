a8f8ec21ea93d37913d9817866cd6a20cf43b5c6
rhys <rhys.puddephatt@btinternet.com>
2018-05-05 19:09:40 +0100

    tweeked inertia, added top speed

--- Assets/Scripts/Player/MovemonetHarness.cs
+++ Assets/Scripts/Player/MovemonetHarness.cs
@@ -11,9 +11,12 @@ public class MovemonetHarness : MonoBehaviour
     [SerializeField]
     float dragAmount = 1;
     [SerializeField]
-    private Vector2 LastDirectionMoved;
+    Vector2 topSpeed = new Vector2(10, 10);
     //onlu global for memory management;
     private Vector2 newVelocity;
+
+    public Vector2 LastDirectionMoved { get; private set; }
+
     // Use this for initialization
     void Awake()
     {
@@ -28,37 +31,21 @@ public class MovemonetHarness : MonoBehaviour
             newVelocity = rbody.velocity;
             //if the direction has changed
 
-            if (LastDirectionMoved.x>0 && direction.x<0 && direction.x !=0)
-            {
-                newVelocity.x = 0;
-            }
             //if the direction has changed and it hasnt changed from zero
-            if (LastDirectionMoved.y > 0 && direction.y < 0 && direction.y != 0)
-            {
-                newVelocity.y = 0;
-            }
+            if ((newVelocity.x > 0 && direction.x < 0) || (newVelocity.x < 0 && direction.x > 0) || direction.x==0)
+                newVelocity.x -= newVelocity.x * dragAmount * Time.fixedDeltaTime;
+
+            //if the direction has changed and it hasnt changed from zero
+            if ((newVelocity.y > 0 && direction.y < 0) || (newVelocity.y < 0 && direction.y > 0) || direction.y == 0)
+                newVelocity.y -= newVelocity.y * dragAmount * Time.fixedDeltaTime;
+
+            newVelocity.x = Mathf.Min(newVelocity.x, topSpeed.x);
+            newVelocity.y = Mathf.Min(newVelocity.y, topSpeed.y);
             rbody.velocity = newVelocity;
             rbody.AddForce(direction * Force * Time.fixedDeltaTime, ForceMode2D.Impulse);
         }
         else
             Debug.LogError("Movement Harness Missing Rigidbody2D", this.gameObject);
-
-        
         LastDirectionMoved = direction;
-    }
-    private void FixedUpdate()
-    {
-        newVelocity = rbody.velocity;
-        if (LastDirectionMoved.x == 0f)
-        {
-          
-            newVelocity.x -= newVelocity.x * dragAmount * Time.fixedDeltaTime;
-        }
-        if (LastDirectionMoved.y == 0)
-            newVelocity.y -= newVelocity.y * dragAmount * Time.fixedDeltaTime;
-
-        rbody.velocity = newVelocity;
-
-
     }
 }
