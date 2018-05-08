c2bdbba25de211e76af2777e07fe0eee6e11c04b
rhys <rhys.puddephatt@btinternet.com>
2018-05-08 03:09:14 +0100

    made health mostly work

--- Assets/HealthDisplay.cs
+++ Assets/HealthDisplay.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HealthDisplay : MonoBehaviour {
-
+    [SerializeField]
+    statusBar HealthBar;
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        if (HealthBar == null)
+            HealthBar = GetComponent<statusBar>();
 	}
-	
+	float calculateNonLiniarHealth(float healthPercentage)
+    {
+        return healthPercentage * healthPercentage;
+    }
 	// Update is called once per frame
-	void Update () {
-		
+	public void UpdateHealth (HealthArgs health) {
+        HealthBar.status = calculateNonLiniarHealth(health.handeler.health/health.handeler.maxHeath);
 	}
 }
